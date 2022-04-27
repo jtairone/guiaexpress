f9ce1b8757248e1861901a4ed4f5b6a82a072c0e
Tairone Morais <jtaironemorais@hotmail.com>
2022-04-27 16:30:44 -0300

    criando pagina de artigos

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -67,7 +67,6 @@ router.get('/admin/categories/edit/:id', (req, res) => {
 router.post('/categories/update', (req, res) => {
     let id = req.body.id;
     let title = req.body.title;
-
     Category.update({title: title, slug: slugify(title)}, {
         where: {
             id: id
